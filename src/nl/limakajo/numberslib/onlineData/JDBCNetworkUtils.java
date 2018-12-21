b88fe04dd048be8df6b8210e7fb4b22c4bc90966
mwbouwkamp <mwbouwkamp@gmail.com>
2018-12-21 18:59:27 +0100

    adapt to change in database definition

--- src/nl/limakajo/numberslib/onlineData/JDBCNetworkUtils.java
+++ src/nl/limakajo/numberslib/onlineData/JDBCNetworkUtils.java
@@ -14,21 +14,22 @@ public class JDBCNetworkUtils {
 
     private static final String TAG = JDBCNetworkUtils.class.getName();
 
-    public static LinkedList<Level> getLevelsJSONFromServer(String tableName) {
+    public static LinkedList<Level> queryLevels(String tableName) {
         LinkedList<Level> toReturn= new LinkedList<>();
-        String SQL = "SELECT * FROM ";
+        String SQL = "SELECT * FROM " + tableName;
         switch (tableName) {
             case NumbersContract.LevelData.TABLE_NAME:
-                SQL += NumbersContract.LevelData.TABLE_NAME;
                 try {
                     Connection connection = getConnection();
                     Statement statement = connection.createStatement();
                     ResultSet resultSet = statement.executeQuery(SQL);
                     while (resultSet.next()) {
-                        String numbersString = resultSet.getString(1);
-                        int averageTime = Integer.parseInt(resultSet.getString(2));
+                        String numbersString = resultSet.getString(2);
+                        int averageTime = Integer.parseInt(resultSet.getString(3));
+                        int timesPlayed = Integer.parseInt(resultSet.getString(4));
                         Level levelToAdd = new Level.LevelBuilder(numbersString)
                                 .setAverageTime(averageTime)
+                                .setTimesPlayed(timesPlayed)
                                 .build();
                         toReturn.add(levelToAdd);
                     }
@@ -40,13 +41,17 @@ public class JDBCNetworkUtils {
                 }
                 break;
             case NumbersContract.CompletedLevelData.TABLE_NAME:
-                SQL += NumbersContract.LevelData.TABLE_NAME;
                 try {
                     Connection connection = getConnection();
                     Statement statement = connection.createStatement();
                     ResultSet resultSet = statement.executeQuery(SQL);
                     while (resultSet.next()) {
-                        //TODO: Logic for CompletedLevelData
+                        String numbersString = resultSet.getString(2);
+                        int userTime = Integer.parseInt(resultSet.getString(3));
+                        Level levelToAdd = new Level.LevelBuilder(numbersString)
+                                .setUserTime(userTime)
+                                .build();
+                        toReturn.add(levelToAdd);
                     }
                     resultSet.close();
                     statement.close();
@@ -61,25 +66,31 @@ public class JDBCNetworkUtils {
         return toReturn;
     }
 
-    public static LinkedList<Level> sendLevelsToServer(LinkedList<Level> levels) {
+    public static LinkedList<Level> insertLevels(String tableName, LinkedList<Level> levels) {
         LinkedList<Level> succesfullySentLevels = new LinkedList<>();
-        String SQL = "INSERT INTO completedleveldata (numbers, usertime) VALUES (?, ?)";
-        try {
-            Connection connection = getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
-            for (Level level: levels) {
-                int affectedrows;
-                preparedStatement.setString(1, level.toString());
-                preparedStatement.setInt(2, level.getUserTime());
-                affectedrows = preparedStatement.executeUpdate();
-                if (affectedrows == 1) {
-                    succesfullySentLevels.add(level);
+        String SQL = "INSERT INTO " + tableName + "(numbers, usertime) VALUES (?, ?)";
+        switch (tableName) {
+            case NumbersContract.CompletedLevelData.TABLE_NAME:
+                try {
+                    Connection connection = getConnection();
+                    PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+                    for (Level level : levels) {
+                        int affectedrows;
+                        preparedStatement.setString(1, level.toString());
+                        preparedStatement.setInt(2, level.getUserTime());
+                        affectedrows = preparedStatement.executeUpdate();
+                        if (affectedrows == 1) {
+                            succesfullySentLevels.add(level);
+                        }
+                    }
+                    preparedStatement.close();
+                    connection.close();
+                } catch (SQLException e) {
+                    System.out.println(e.getMessage());
                 }
-            }
-            preparedStatement.close();
-            connection.close();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+                break;
+            default:
+                throw new UnsupportedOperationException("Unsupported tableName: " + tableName);
         }
         return succesfullySentLevels;
     }
